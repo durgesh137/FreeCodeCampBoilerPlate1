227f5fe604a1b8267c01890d8176453545bb0827
durgesh <kdurgesh137@gmail.com>
2022-07-05 13:18:26 +0530

    add hello express

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 let express = require('express');
 let app = express();
 
-
-app.get('/',(request, response)=>{
     console.log('Hello World');
-    response.status(200).send("Hello World");
+app.get('/',(request, response)=>{
+
+    response.status(200).send("Hello Express");
   })
   
 
