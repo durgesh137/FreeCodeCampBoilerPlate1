d5d3704e8cddfb58133ba5649ebca2d500a9be74
durgesh <kdurgesh137@gmail.com>
2022-07-05 13:45:20 +0530

    HTML file returned

--- myApp.js
+++ myApp.js
@@ -1,11 +1,21 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const path = require('path');//to get path of html file
+const app = express();
 
-app.get('/',(request, response)=>{
-
-    response.status(200).send("Hello Express");
-  })
+/**
+ * Task 2 done, sending text with get request
+ */
+// app.get('/',(request, response)=>{
+//     response.status(200).send("Hello Express");
+//   })
   
+/**
+ * Task 3, serve html file, done
+ */
+app.get('/',(request, response)=>{
+    console.log(path.join(__dirname));
+    response.status(200).sendFile(path.join(__dirname,"/views/index.html"));
+});
 
 
 
